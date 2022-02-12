dc75ca337b10030fb3a6349d49698739562a10f8
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-12 17:22:21 +0530

    Update mute.py

--- annabelle/Plugins/mute.py
+++ annabelle/Plugins/mute.py
@@ -1,7 +1,9 @@
+import time
 from userbot import Annabelle
 from pyrogram import filters as vrn
 from pyrogram.types import ChatPermissions
 from config import HANDLER
+from annabelle.helper_funcs.strings import MUTE.TXT
 
 import logging
 logging = logging.getLogger(__name__)
@@ -19,7 +21,7 @@ async def mute(Annabelle, message):
         reason = ''
       try:
         await Annabelle.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, ChatPermissions())
-        await message.edit(MUTE.TXT.format(message.reply_to_message.from_user.mention, message.chat.title, reason))
+        await message.edit(MUTE.TXT.format(message.reply_to_message.from_user.mention, message.chat.title, "forever" reason))
       except Exception as e:
         await message.edit("`Failed to mute that user! Check app logs for more..`")
         print(e)
@@ -27,3 +29,29 @@ async def mute(Annabelle, message):
       await message.edit("`Whom to mute dude? Reply to the message of the user to mute`")
   else:
     await message.edit("`We are not admin here to do that!`")
+
+@Annabelle.on_message(vrn.command('tmute', HANDLER) & vrn.group & vrn.outgoing)
+async def tmute(Annabelle, message):
+  if message.reply_to_message:
+    mem = await Annabelle.get_chat_member(message.chat.id, message.from_user.id)
+    if mem.status in ['administrator', 'creator']:
+                      args =message.text.split(None, 1)
+                      if len(args) >= 2:
+                        if type(args[1]) is int:
+                             samayam = args[1]*60
+                             try:
+                                await Annabelle.restrict_chat_member(message.chat.id, message.reply_to_message, ChatPermissions(), int(time() + samayam))
+                             except Exception as e:
+                                await message.edit("`failed to mute! See app logs`")
+                                print(e)
+                        else:
+                          await message.edit("`time must be integer`")
+                      else:
+                        await message.edit("`specify a time`")
+     else:
+       await message.edit("`We are not admin here`")
+   else:
+    await message.edit("`Reply to a message`")
+                                
+                                
+                                
