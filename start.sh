ae4b382fe1e7e34c3fc9fd637cf90a807a73dc61
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-05-06 06:05:07 +0530

    🫠

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Annabelle"
 git clone https://github.com/JoelBobanOffline/Annabelle
 cd Annabelle
-pip3 -r install requirements.txt
+pip3 install -r requirements.txt
 echo "Starting!"
 python3 bot.py
 python3 annabelle.py
